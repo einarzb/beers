f5f33a1a20e726e74ff3311da360e9e290e115f0
einarzb <einarzb@gmail.com>
2017-03-13 22:21:51 +0200

    did some commenting

--- public/js/services/beers.js
+++ public/js/services/beers.js
@@ -1,23 +1,23 @@
 app.factory('beerService', function($http, $rootScope){
-         
-     
-  var beerFactory = {beersList: []}; //turned my array onto an object that holds an empty array
+        
+  var beerFactory = {beersList: []}; 
   
   //fetching all beers from server API
+
   beerFactory.getBeers = function(){
-    return $http.get('/beers') //returns promise
-    //success
-      .then(function(response){ 
-        // console.log(response.data)
-          //copies the data that was received into our beers array
-          angular.copy(response.data, beerFactory.beersList); 
-          // beerFactory.beersList.push({});
-      }, 
-      //error
-      function(err){ 
+    return $http.get('/beers')//returns promise
+    //if success
+    .then(function(response){ 
+    console.log(beerFactory.beersList);  
+    //copies the data that was received into our beers array
+    angular.copy(response.data, beerFactory.beersList); 
+   // beerFactory.beersList.push({});
+    }, 
+   //if error
+    function(err){ 
         console.error(err)
       });
-  }; //end getBeers
+  }; 
 
   beerFactory.addToList = function(newBeer){
      console.log('from the service')
