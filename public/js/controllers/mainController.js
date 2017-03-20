717d1cf6aef1389acff1cf5c98c0110bd7f98485
einarzb <einarzb@gmail.com>
2017-03-20 10:31:05 +0200

    the edit view works

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -7,6 +7,10 @@ app.controller('mainController', function($scope, beerFactory) {
   $scope.image = "https://s-media-cache-ak0.pinimg.com/736x/49/1a/f0/491af06b6e11eb0ce4cde23626deb480.jpg";
   $scope.rate;
 
+  $scope.editable = false; //hides input fields
+  $scope.beerView = false; 
+
+
 //beer array
   $scope.beers = beerFactory.beers;
 
@@ -34,7 +38,14 @@ app.controller('mainController', function($scope, beerFactory) {
 $scope.removeFromList = beerFactory.removeFromList;
 
 //invoke service function to edit beers
-$scope.editItem = beerFactory.editItem;
+$scope.editItem = function(){
+  $scope.editable = true;
+  console.log("im inside editItem function. yeahy!");
+  $scope.beerView = true; //hides original view 
+}
+
+
+$scope.editedBeer = beerFactory.editedBeer;
 
 //invoking 
     beerFactory.getBeers();
